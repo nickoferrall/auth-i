2b8f4e3ef99cf6974abca6c402306bd9a1e6f487
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-21 15:53:00 -0500

    Refactored with async

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -3,43 +3,35 @@ const router = express.Router();
 const db = require('../database/dbConfig');
 const bcrypt = require('bcryptjs');
 
-router.post('/register', (req, res) => {
-  const creds = req.body;
-  const hash = bcrypt.hashSync(creds.password, 14);
-  creds.password = hash;
-  db('users')
-    .insert(creds)
-    .then(ids => {
-      const id = ids[0];
-      req.session.username = user.username;
-      res.status(201).json({ newUserId: id });
-    })
-    .catch(err => res.status(500).json(err));
+router.post('/register', async (req, res) => {
+  try {
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 14);
+    creds.password = hash;
+    const ids = await db('users').insert(creds);
+    const id = ids[0];
+    req.session.username = user.username;
+    res.status(201).json({ newUserId: id });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/register', (req, res) => {
-  db('users')
-    .select('id', 'username', 'password')
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.status(err));
-});
-
-router.post('/login', (req, res) => {
-  const creds = req.body;
-  db('users')
-    .where({ username: creds.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        req.session.username = user.username; // on login, any info that we want stored to the session, we add to the req.session object
-        res.status(200).json({ message: 'Logged in' });
-      } else {
-        res.status(401).json({ message: 'You shall not pass!' });
-      }
-    })
-    .catch(err => res.status(500).json(err));
+router.post('/login', async (req, res) => {
+  try {
+    const creds = req.body;
+    const user = await db('users')
+      .where({ username: creds.username })
+      .first();
+    if (user && bcrypt.compareSync(creds.password, user.password)) {
+      req.session.username = user.username; // on login, any info that we want stored to the session, we add to the req.session object
+      res.status(200).json({ message: 'Logged in' });
+    } else {
+      res.status(401).json({ message: 'You shall not pass!' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get('/logout', (req, res) => {
@@ -63,31 +55,21 @@ function protected(req, res, next) {
   }
 }
 
-router.get('/users', protected, (req, res) => {
-  if (req.session.username) {
-    // do the below but as middleware
-    db('users')
-      .select('id', 'username', 'password')
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => res.status(500).send(err));
-  } else {
-    res.status(401).send('Not authorised.');
+router.get('/users', protected, async (req, res) => {
+  try {
+    const users = await db('users').select('id', 'username', 'password');
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(users);
   }
 });
 
-router.get('/restricted/*', protected, (req, res) => {
-  if (req.session.username) {
-    // do the below but as middleware
-    db('users')
-      .select('id', 'username', 'password')
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => res.status(500).send(err));
-  } else {
-    res.status(401).send('Not authorised.');
+router.get('/restricted/*', protected, async (req, res) => {
+  try {
+    const users = await db('users').select('id', 'username', 'password');
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(err);
   }
 });
 
